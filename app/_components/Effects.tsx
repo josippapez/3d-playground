6347110c873f79817332640bfe8201ffcd24777b
Josip Papež <josip.papez@prototyp.digital>
2023-09-27 18:11:53 +0200

    Changed prop name

--- app/_components/Effects.tsx
+++ app/_components/Effects.tsx
@@ -2,16 +2,16 @@ import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import { useControls } from 'leva';
 
 export function Effects() {
-  const { enabled, intensity, luminanceSmoothing, luminanceThreshold } =
+  const { enableFX, intensity, luminanceSmoothing, luminanceThreshold } =
     useControls({
-      enabled: true,
+      enableFX: true,
       luminanceThreshold: { value: 4, min: 0, max: 4, step: 0.05 },
       luminanceSmoothing: { value: 50, min: 0, max: 50, step: 1 },
       intensity: { value: 0.09, min: 0, max: 1, step: 0.01 },
     });
 
   return (
-    enabled && (
+    enableFX && (
       <EffectComposer multisampling={16} resolutionScale={1} renderPriority={2}>
         <Bloom
           luminanceThreshold={luminanceThreshold}
